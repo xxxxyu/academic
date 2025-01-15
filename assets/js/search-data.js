4bedac803706aea0f1397955f57886a78bec24f7
xxxxyu <xiangyu.sdlc@foxmail.com>
2025-01-15 03:08:58 +0000

    Deploying to gh-pages from @ xxxxyu/academic@1cbb7e5fc33a6483f2bffb2bf61b850ffa6176bf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,36 +30,14 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/academic/cv/";
           },
-        },{id: "nav-blog",
-          title: "Blog",
+        },{id: "nav-blog-lt-i-class-quot-fa-solid-fa-arrow-up-right-from-square-quot-gt-lt-i-gt",
+          title: "Blog &lt;i class=&quot;fa-solid fa-arrow-up-right-from-square&quot;&gt;&lt;/i&gt;",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/academic/blog/";
           },
-        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-      
-        title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-        
-      },
-    },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-      
-        title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-        
-      },
-    },{id: "news-our-paper-flexnn-efficient-and-adaptive-dnn-inference-on-memory-constrained-edge-devices-is-conditionally-accepted-by-mobicom-2024-thanks-to-all-the-coauthors-yuanchun-li-yuanzhe-li-ting-cao-and-yunxin-liu-clap",
+        },{id: "news-our-paper-flexnn-efficient-and-adaptive-dnn-inference-on-memory-constrained-edge-devices-is-conditionally-accepted-by-mobicom-2024-thanks-to-all-the-coauthors-yuanchun-li-yuanzhe-li-ting-cao-and-yunxin-liu-clap",
           title: 'Our paper “FlexNN: Efficient and Adaptive DNN Inference on Memory-Constrained Edge Devices” is...',
           description: "",
           section: "News",},{id: "news-our-position-amp-amp-survey-paper-on-mobile-llm-agents-personal-llm-agents-insights-and-survey-about-the-capability-efficiency-and-security-is-released-sparkles-arxiv-github-机器之心",
